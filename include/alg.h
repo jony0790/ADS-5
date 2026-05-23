70149c871fe7c4947dfb6cd884adc993a9ea2774
Джалоладдин <joniatabekovich07@gmail.com>
2026-05-23 12:33:32 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,10 +1,12 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 
 #include <string>
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
+// infix -> postfix
+std::string infx2pstfx(const std::string& inf);
+
+// postfix evaluation
+int eval(const std::string& post);
 
 #endif  // INCLUDE_ALG_H_
