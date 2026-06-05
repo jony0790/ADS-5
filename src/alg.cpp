8b159101cded63220451411fb5fee88335fbab27
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-05 19:37:31 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,8 @@
-#include <string>
+// Copyright 2026
+
 #include <cctype>
+#include <string>
+
 #include "../include/tstack.h"
 
 int priority(char op) {
@@ -12,31 +15,30 @@ int priority(char op) {
     return 0;
 }
 
-// infiks -> postfiks
 std::string infx2pstfx(const std::string& inf) {
     TStack<char, 100> st;
     std::string result;
 
-    for (size_t i = 0; i < inf.size();) {
-
+    for (size_t i = 0; i < inf.size(); ) {
         if (isspace(inf[i])) {
             ++i;
             continue;
         }
 
         if (isdigit(inf[i])) {
-            while (i < inf.size() && isdigit(inf[i])) {
+            while (i < inf.size() &&
+                   isdigit(inf[i])) {
                 result += inf[i];
                 ++i;
             }
+
             result += ' ';
-        }
-        else if (inf[i] == '(') {
+        } else if (inf[i] == '(') {
             st.push('(');
             ++i;
-        }
-        else if (inf[i] == ')') {
-            while (!st.empty() && st.top() != '(') {
+        } else if (inf[i] == ')') {
+            while (!st.empty() &&
+                   st.top() != '(') {
                 result += st.top();
                 result += ' ';
                 st.pop();
@@ -46,14 +48,12 @@ std::string infx2pstfx(const std::string& inf) {
                 st.pop();
 
             ++i;
-        }
-        else {
+        } else {
             char op = inf[i];
 
             while (!st.empty() &&
                    st.top() != '(' &&
-                   priority(st.top()) >= priority(op))
-            {
+                   priority(st.top()) >= priority(op)) {
                 result += st.top();
                 result += ' ';
                 st.pop();
@@ -70,18 +70,18 @@ std::string infx2pstfx(const std::string& inf) {
         st.pop();
     }
 
-    if (!result.empty() && result.back() == ' ')
+    if (!result.empty() &&
+        result.back() == ' ') {
         result.pop_back();
+    }
 
     return result;
 }
 
-// postfiks ifodani hisoblash
 int eval(const std::string& post) {
     TStack<int, 100> st;
 
-    for (size_t i = 0; i < post.size();) {
-
+    for (size_t i = 0; i < post.size(); ) {
         if (isspace(post[i])) {
             ++i;
             continue;
@@ -90,14 +90,14 @@ int eval(const std::string& post) {
         if (isdigit(post[i])) {
             int num = 0;
 
-            while (i < post.size() && isdigit(post[i])) {
+            while (i < post.size() &&
+                   isdigit(post[i])) {
                 num = num * 10 + (post[i] - '0');
                 ++i;
             }
 
             st.push(num);
-        }
-        else {
+        } else {
             int b = st.top();
             st.pop();
 
@@ -108,15 +108,12 @@ int eval(const std::string& post) {
                 case '+':
                     st.push(a + b);
                     break;
-
                 case '-':
                     st.push(a - b);
                     break;
-
                 case '*':
                     st.push(a * b);
                     break;
-
                 case '/':
                     st.push(a / b);
                     break;
